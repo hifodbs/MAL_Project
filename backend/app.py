820cbfaab85d6c715289c1b1b629519cc8e0c738
Leo <gioleo999@gmail.com>
2025-12-22 17:49:33 +0100

    feat(backend): expose plants API

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from routes.test_endpoint import predictions_bp
+from routes.plants import plants_bp
 
 app = Flask(__name__)
-app.register_blueprint(predictions_bp)
+
+#attaches routes defined in (...) and becomes active
+app.register_blueprint(predictions_bp) 
+app.register_blueprint(plants_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
