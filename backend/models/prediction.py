4de6f4bc542d5fb44c6cb07fa34d269dcc96e229
Leo <gioleo999@gmail.com>
2026-01-17 11:39:51 +0100

    feat(models): distinguish between Global and Panel

--- backend/models/prediction.py
+++ backend/models/prediction.py
@@ -2,8 +2,15 @@ from dataclasses import dataclass
 from datetime import datetime
 
 @dataclass
-class Prediction:
+class PanelPrediction:
     timestamp: datetime
     plant_id: str
     panel_id: str
+    ac_power: float
+
+
+@dataclass
+class GlobalPrediction:
+    timestamp: datetime
+    plant_id: str
     ac_power: float
