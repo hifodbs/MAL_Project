83cdc30e209906e706facfd016989c2a37ebb464
Leo <gioleo999@gmail.com>
2026-01-19 16:31:42 +0100

    feat(bakcend): expose panels API

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from backend.routes.test_endpoint import predictions_bp
 from backend.routes.plants import plants_bp
+from backend.routes.panels import panels_bp
 
 app = Flask(__name__)
 
 #attaches routes defined in (...) and becomes active
 app.register_blueprint(predictions_bp) 
 app.register_blueprint(plants_bp)
+app.register_blueprint(panels_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
