5ec1250c9aab0b8a4ca542814df7aadddaa3406a
Leo <gioleo999@gmail.com>
2026-01-24 15:50:32 +0100

    fix(service): bug in plants_service

--- backend/services/plants_service.py
+++ backend/services/plants_service.py
@@ -1,7 +1,7 @@
 # services/prediction_service.py
 from backend.dao.plant_dao import PlantsDAO
 from backend.dao.measurements_dao import MeasurementsDAO
-from datetime import datetime, timedelta
+from datetime import datetime
 
 
 class PlantsService:
@@ -15,5 +15,5 @@ class PlantsService:
     def get_global_measurements_by_plant_id(self, plant_id: str):
         return self.measurements_dao.get_all_global_measurements_by_plant_id(plant_id=plant_id)
     
-    def get_global_measurements_by_plant_id_and_time_range(self, plant_id: str, start_time: datetime, hours: int):
-        return self.measurements_dao.get_global_measurements_by_plant_id_and_time_range(plant_id=plant_id, start_time=start_time, hours=hours)
+    def get_global_measurements_by_plant_id_and_time_range(self, plant_id: str, end_time: datetime, hours: int):
+        return self.measurements_dao.get_global_measurements_by_plant_id_and_time_range(plant_id=plant_id, end_time=end_time, hours=hours)
