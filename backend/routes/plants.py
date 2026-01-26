c38dd2b06ae356b63a9663c153fd56400c867c78
Leo <gioleo999@gmail.com>
2026-01-26 12:26:27 +0100

    feat(routes): get_prediction_service to get models from app contest, add # GET /plants/<plant_id>/report for future implementation

--- backend/routes/plants.py
+++ backend/routes/plants.py
@@ -1,15 +1,25 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, current_app
 from backend.services.plants_service import PlantsService
 from backend.services.panels_service import PanelsService
-import random
+from backend.services.prediction_service import PredictionService
 from datetime import datetime
 
 plants_bp = Blueprint("plants", __name__)
 
 
 data_directory = "cleaned_data"
+historical_predictions = "historical_predictions"
 panles_service = PanelsService(data_directory)
 plants_service = PlantsService(data_directory)
+
+
+def get_prediction_service():
+    return PredictionService(
+        models=current_app.models,
+        data_directory=current_app.config["DATA_DIRECTORY"],
+        historical_predictions=current_app.config["HISTORICAL_PREDICTIONS"],
+    )
+
 
 # GET /plants
 
@@ -41,29 +51,34 @@ def plant_predictions(plant_id):
     #Returns a list of predictions for a specific plant.
     #Each prediction: {"timestamp": ISO8601 string, "plant_id": string, "ac_power": float}
     
-    hours = request.args.get("hours", default=48, type=int) 
-    starting_time = request.args.get("time", default="2020-06-01T00:00:00")
+    start_time_str = request.args.get("start_time", default=None)
+    end_time_str = request.args.get("end_time", default="2020-06-14T10:45:00")
+
+    if start_time_str:
+        try:
+            start_time = datetime.fromisoformat(start_time)
+        except ValueError:
+            return jsonify({"error": "Invalid time format. Use ISO 8601."}), 400
+    else:
+        start_time = None
+
+    if end_time_str:
+        try:
+            end_time = datetime.fromisoformat(end_time_str)
+        except ValueError:
+            return jsonify({"error": "Invalid time format. Use ISO 8601."}), 400
+    else:
+        end_time = None 
 
     try:
-        starting_time = datetime.fromisoformat(starting_time)
-    except ValueError:
-        return jsonify({"error": "Invalid time format. Use ISO 8601."}), 400
-
-    try:
-
-        #this predictions are fake 
-        predictions = plants_service.get_global_measurements_by_plant_id_and_time_range(plant_id, starting_time, hours)
-        if not predictions:
-            return jsonify({"error": f"No predictions found for plant {plant_id}"}), 404
-        return jsonify([
-        {
-            "timestamp": p.timestamp.isoformat(),
-            "plant_id": p.plant_id,
-            "ac_power": p.ac_power * random.uniform(0.5, 1.5),
-        }
-        for p in predictions
-    ]), 200
-    except Exception as e:
+        prediction_service = get_prediction_service()
+        predictions = prediction_service.predict_plant(
+            plant_id=plant_id,
+            start_time=start_time,
+            end_time=end_time
+        )
+        return jsonify(predictions), 200
+    except ValueError as e:
         return jsonify({"error": str(e)}), 500
 
 
@@ -74,29 +89,54 @@ def plant_measurements(plant_id):
     
     #Returns historical measurements for a plant.
     
-    hours = request.args.get("hours", default=24, type=int) 
-    starting_time = request.args.get("time", default="2020-05-31T00:00:00")
+    start_time_str = request.args.get("start_time", default=None)
+    end_time_str = request.args.get("end_time", default="2020-06-14T10:45:00")
+
+    if start_time_str:
+        try:
+            start_time = datetime.fromisoformat(start_time)
+        except ValueError:
+            return jsonify({"error": "Invalid time format. Use ISO 8601."}), 400
+    else:
+        start_time = None
+
+    if end_time_str:
+        try:
+            end_time = datetime.fromisoformat(end_time_str)
+        except ValueError:
+            return jsonify({"error": "Invalid time format. Use ISO 8601."}), 400
+    else:
+        end_time = None 
 
     try:
-        starting_time = datetime.fromisoformat(starting_time)
+        measurements = plants_service.get_global_measurements_by_plant_id_and_time_range(plant_id, end_time, start_time)
+        if not measurements:
+            return jsonify({"error": f"No measurements found for plant {plant_id}"}), 404
+        return jsonify([{ "timestamp": m.timestamp.isoformat(), "plant_id": m.plant_id, "ac_power": m.ac_power} for m in measurements]), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+
+
+# GET /plants/<plant_id>/report
+
+@plants_bp.route("/plants/<plant_id>/report", methods=["GET"])
+def plant_report(plant_id, day):
+    
+    day = request.args.get("day",default="2020-06-14T10:45:00")
+
+    try:
+        day = request.args.get("day",default="2020-06-14T10:45:00")
     except ValueError:
         return jsonify({"error": "Invalid time format. Use ISO 8601."}), 400
 
     try:
-        measurements = plants_service.get_global_measurements_by_plant_id_and_time_range(plant_id, starting_time, hours)
-        if not measurements:
-            return jsonify({"error": f"No measurements found for plant {plant_id}"}), 404
-        return jsonify([
-        {
-            "timestamp": m.timestamp.isoformat(),
-            "plant_id": m.plant_id,
-            "ac_power": m.ac_power,
-        }
-        for m in measurements
-    ]), 200
+        report = plants_service.generate_report(plant_id, day)
+        return jsonify(report), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-    
+
 
 ##testing
 #
