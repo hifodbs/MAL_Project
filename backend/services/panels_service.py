24084264882865ff4f33b8e0d55d5c521fb879bf
Leo <gioleo999@gmail.com>
2026-01-26 12:17:45 +0100

    fix(service): inputs inside panles_service functions compatible with dao

--- backend/services/panels_service.py
+++ backend/services/panels_service.py
@@ -10,19 +10,19 @@ class PanelsService:
 
 
     def get_all_panel_measurements_by_id(self, plant_id: str, panel_id: str):
-        return self.measurements_dao.get_all_panel_measurements_by_panel_id(plant_id=plant_id, panel_id=panel_id)
+        return self.measurements_dao.get_all_panel_measurements_by_plant_id_and_panel_id(plant_id=plant_id, panel_id=panel_id)
 
 
-    def get_all_panel_measurements_by_id_and_time_reange(self, plant_id: str, panel_id: str, end_time: datetime, hours: int):
-        return self.measurements_dao.get_panel_measurements_by_panel_id_and_time_range(plant_id=plant_id, panel_id=panel_id, end_time=end_time, hours=hours)
+    def get_all_panel_measurements_by_id_and_time_reange(self, plant_id: str, panel_id: str, start_time: datetime = None, end_time: datetime = None):
+        return self.measurements_dao.get_panel_measurements_by_panel_id_and_time_range(plant_id=plant_id, panel_id=panel_id, start_time=start_time, end_time=end_time)
     
 
     def get_all_panel_measurements_by_plant_id(self, plant_id: str):
         return self.measurements_dao.get_all_panel_measurements_by_plant_id(plant_id=plant_id)
 
 
-    def get_all_panel_mesurements_by_plant_id_and_time_renage(self, plant_id: str, end_time: datetime, hours: int):
-        return self.measurements_dao.get_panel_measurements_by_plant_id_and_time_range(plant_id=plant_id, end_time=end_time, hours=hours)
+    def get_all_panel_mesurements_by_plant_id_and_time_renage(self, plant_id: str, start_time: datetime = None, end_time: datetime = None):
+        return self.measurements_dao.get_panel_measurements_by_plant_id_and_time_range(plant_id=plant_id, start_time=start_time, end_time=end_time)
 
 
     def get_all_panel_measurements(self):
