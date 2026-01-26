cdfbcfffc014cf3080a7f999ba82b7be1fc4c3f7
Leo <gioleo999@gmail.com>
2026-01-26 12:23:02 +0100

    fix(service): typo

--- backend/services/weather_service.py
+++ backend/services/weather_service.py
@@ -1,11 +1,11 @@
-from backend.dao.weather_dao import WeatheDAO
+from backend.dao.weather_dao import WeatherDAO
 from backend.dao.measurements_dao import MeasurementsDAO
 from datetime import datetime, timedelta
 
 
 class WeatherService:
     def __init__(self, data_directory="cleaned_data"):
-        self.weather_dao = WeatheDAO(data_directory=data_directory)
+        self.weather_dao = WeatherDAO(data_directory=data_directory)
 
 
     def get_all(self):
