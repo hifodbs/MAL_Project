12270b648ec8e8f86a5cc70c52150a8d9fd4ceab
Leo <gioleo999@gmail.com>
2026-01-27 14:27:45 +0100

    feat(frontend): add caching in api, add get_report, add get_drift_summary_by_plant_id

--- frontend/api.py
+++ frontend/api.py
@@ -1,7 +1,9 @@
 import requests
+import streamlit as st
 
 BASE_URL = "http://127.0.0.1:5000"  # Flask backend
 
+@st.cache_data(ttl=600)
 def get_plants():
     """
     Fetch plants from the Flask API.
@@ -18,9 +20,10 @@ def get_plants():
         return response.json()
     except requests.RequestException as e:
         print(f"Error fetching plants: {e}")
-        return 
+        return []
     
-def get_predictions_by_plant_id(plant_id, time):
+@st.cache_data(ttl=600)
+def get_predictions_by_plant_id(plant_id, start_time = None, end_time = None):
     """
     Fetch predictions for a specific plant from the Flask API.
 
@@ -35,16 +38,22 @@ def get_predictions_by_plant_id(plant_id, time):
     """
     try:
         params = {}
-        if time is not None:
-            params["time"] = time
-        response = requests.get(f"{BASE_URL}/plants/{plant_id}/predictions")
+        if start_time is not None:
+            params["start_time"] = start_time
+        if end_time is not None:
+            params["end_time"] = end_time
+
+        response = requests.get(f"{BASE_URL}/plants/{plant_id}/predictions", params=params)
         response.raise_for_status()
+
         return response.json()
+    
     except requests.RequestException as e:
         print(f"Error fetching predictions for plant {plant_id}: {e}")
         return []
-    
-def get_measurements_by_plant_id(plant_id, time = None):
+
+@st.cache_data(ttl=600)
+def get_measurements_by_plant_id(plant_id, start_time = None, end_time = None):
     """
     Fetch measurements for a specific plant from the Flask API.
 
@@ -59,78 +68,123 @@ def get_measurements_by_plant_id(plant_id, time = None):
     """
     try:
         params = {}
-        if time is not None:
-            params["time"] = time
-        response = requests.get(f"{BASE_URL}/plants/{plant_id}/measurements")
+        if start_time is not None:
+            params["start_time"] = start_time
+        if end_time is not None:
+            params["end_time"] = end_time
+
+        response = requests.get(f"{BASE_URL}/plants/{plant_id}/measurements", params=params)
         response.raise_for_status()
+
         return response.json()
+    
     except requests.RequestException as e:
         print(f"Error fetching predictions for plant {plant_id}: {e}")
         return []
 
+@st.cache_data(ttl=600)
 def get_panels_by_plant_id(plant_id):
     try:
         response = requests.get(f"{BASE_URL}/plants/{plant_id}/panels")
         response.raise_for_status()
+
         return response.json()
+    
     except requests.RequestException as e:
         print(f"Error fetching panels for plant {plant_id}: {e}")
         return []
 
-def get_meaurements_by_panel_id(plant_id, panel_id, time):
+@st.cache_data(ttl=600)
+def get_measurements_by_panel_id(plant_id, panel_id, start_time = None, end_time = None):
     try:
         params = {}
-        if time is not None:
-            params["time"] = time
-        response = requests.get(f"{BASE_URL}/plants/{plant_id}/panels/{panel_id}/measurements")
+        if start_time is not None:
+            params["start_time"] = start_time
+        if end_time is not None:
+            params["end_time"] = end_time
+
+        response = requests.get(f"{BASE_URL}/plants/{plant_id}/panels/{panel_id}/measurements", params=params)
         response.raise_for_status()
+
         return response.json()
+    
     except requests.RequestException as e:
         print(f"Error fetching measurements for panel {panel_id}: {e}")
         return []
     
-def get_predictions_by_panel_id(plant_id, panel_id, time=None):
+@st.cache_data(ttl=600)
+def get_predictions_by_panel_id(plant_id, panel_id, start_time = None, end_time= None):
     try:
         params = {}
-        if time is not None:
-            params["time"] = time
-        response = requests.get(f"{BASE_URL}/plants/{plant_id}/panels/{panel_id}/predictions")
+        if start_time is not None:
+            params["start_time"] = start_time
+        if end_time is not None:
+            params["end_time"] = end_time
+        
+        response = requests.get(f"{BASE_URL}/plants/{plant_id}/panels/{panel_id}/predictions", params=params)
         response.raise_for_status()
+
         return response.json()
+    
     except requests.RequestException as e:
         print(f"Error fetching predictions for panel {panel_id}: {e}")
         return []
 
+@st.cache_data(ttl=600)
 def get_new_prediction_by_panel_id(plant_id, panel_id, time=None):
     try:
         params = {}
         if time is not None:
             params["time"] = time 
 
-        response = requests.get(
-            f"{BASE_URL}/plants/{plant_id}/panels/{panel_id}/new_prediction",
-            params=params
-        )
+        response = requests.get(f"{BASE_URL}/plants/{plant_id}/panels/{panel_id}/new_prediction",params=params)
         response.raise_for_status()
+
         return response.json()
 
     except requests.RequestException as e:
-        print(f"Error fetching predictions for panel {panel_id}: {e}")
+        print(f"Error gettin prediction for panel {panel_id}: {e}")
         return []
     
+@st.cache_data(ttl=600)
 def get_new_prediction_by_plant_id(plant_id, time=None):
     try:
         params = {}
         if time is not None:
             params["time"] = time 
 
-        response = requests.get(
-            f"{BASE_URL}/plants/{plant_id}/new_prediction",
-            params=params
-        )
+        response = requests.get(f"{BASE_URL}/plants/{plant_id}/new_prediction",params=params)
         response.raise_for_status()
+
         return response.json()
 
     except requests.RequestException as e:
-        print(f"Error fetching predictions for panel {plant_id}: {e}")
+        print(f"Error gatting prediction for panel {plant_id}: {e}")
         return []
+    
+
+@st.cache_data(ttl=600)
+def get_report(plant_id, day=None):
+    
+    if day is None:
+        return None
+    try:
+        params = {"day": day}
+        response = requests.get(f"{BASE_URL}/plants/{plant_id}/report", params=params)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Error fetching report for plant {plant_id}: {e}")
+        return None
+    
+
+@st.cache_data(ttl=600)
+def get_drift_summary_by_plant_id(plant_id, start_time=None, end_time=None):
+    try:
+        params = {"start_time": start_time, "end_time": end_time}
+        response = requests.get(f"{BASE_URL}/plants/{plant_id}/drift_summary", params=params)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print(f"Error fetching drift summary: {e}")
+        return {}
