fcea261842ea1f2e2ef2acc6da5d8d87c4203ef5
Leo <gioleo999@gmail.com>
2026-01-28 09:43:06 +0100

    fix(service): add missing relative path to LSTM dao initialization

--- backend/services/panels_service.py
+++ backend/services/panels_service.py
@@ -7,7 +7,7 @@ class PanelsService:
     def __init__(self, data_directory="cleaned_data"):
         self.panel_dao = PanelsDAO(data_directory=data_directory)
         self.measurements_dao = MeasurementsDAO(data_directory=data_directory)
-        self.LSTM_measurements_dao = MeasurementsDAO(data_directory="")
+        self.LSTM_measurements_dao = MeasurementsDAO(data_directory="InclLSTM")
 
 
     def get_all_panel_measurements_by_id(self, plant_id: str, panel_id: str):
