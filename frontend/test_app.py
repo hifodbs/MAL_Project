775a854d7a94ed1b08a0b795035f12405407c5dc
Leo <gioleo999@gmail.com>
2026-01-28 16:16:42 +0100

    refactor: remove old unused code

--- frontend/test_app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from api import get_predictions
-
-st.title("Temperature Predictions")
-
-data = get_predictions()
-st.line_chart({"Real": data["real"], "Predicted": data["predicted"]})
